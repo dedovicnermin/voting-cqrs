0002c5dbd3b1beb0430ed25ad790d360f9aa4c96
Nermin Dedovic <dedovicnermin@outlook.com>
2024-02-27 12:09:02 -0600

    feature/react_rest_client:
    Finalizing election view footer

--- client/src/component/election/ElectionFoot.js
+++ client/src/component/election/ElectionFoot.js
@@ -9,16 +9,29 @@ export default function ElectionFoot({election}) {
     // const eventKey = `${user.id}:${election.id}`;
     const [selectedCandidate, setSelectedCandidate] = useState();
 
+    /**
+     * Update selectedCandidate state when user selects a value from drop-down
+     * @param event containing drop-down current value
+     */
     const handleSelectChange = event => {
         setSelectedCandidate(event.currentTarget.value);
     }
+
+    /**
+     * Invoked when form button has been clicked
+     * @param event formEvent
+     */
     const handleSubmitVote = event => {
         event.preventDefault()
-        console.log("SUBMIT")
         alert(`Vote request for '${selectedCandidate}' successfully sent`)
         navigate("/elections");
     }
 
+    /**
+     * Conditional for displaying submit vote button
+     * Do not allow vote to be sent if a candidate from election was not selected
+     * @returns {boolean}
+     */
     const displayVoteButton = () => selectedCandidate !== undefined &&
         selectedCandidate !== "" &&
         selectedCandidate !== DEFAULT_SELECTION;
@@ -26,18 +39,22 @@ export default function ElectionFoot({election}) {
 
     return (
         <Container className="election_foot">
+            <div id="election_foot-title"><h3>VOTE</h3></div>
             <Form onSubmit={handleSubmitVote}>
-                <Form.Label>Vote for election</Form.Label>
-                <Form.Select onChange={handleSelectChange} value={selectedCandidate}>
-                    <option key={DEFAULT_SELECTION} value={DEFAULT_SELECTION}>{DEFAULT_SELECTION}</option>
-                    {
-                        Object.keys(election.candidates).map(candidate =>
-                            <option key={candidate} value={candidate}>{candidate}</option>
-                        )
-                    }
-                </Form.Select>
-                <Button variant="primary" disabled={displayVoteButton() === false} type="submit">Request Vote</Button>
-
+                <Form.Group controlId="formSelectCandidate">
+                    <Form.Select onChange={handleSelectChange} value={selectedCandidate}>
+                        <option key={DEFAULT_SELECTION} value={DEFAULT_SELECTION}>{DEFAULT_SELECTION}</option>
+                        {
+                            Object.keys(election.candidates).map(candidate =>
+                                <option key={candidate} value={candidate}>{candidate}</option>
+                            )
+                        }
+                    </Form.Select>
+                    <Form.Text muted>Only one vote will be counted per election</Form.Text>
+                </Form.Group>
+                <div id="election_foot-button">
+                    <Button variant="primary" disabled={displayVoteButton() === false} type="submit" size="md">Submit</Button>
+                </div>
             </Form>
         </Container>
     )
