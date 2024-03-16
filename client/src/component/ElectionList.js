2377fc29a8932b092df66d74582054dcf9a18ed9
Nermin Dedovic <dedovicnermin@outlook.com>
2024-03-16 17:26:18 -0500

    feature/crunchyroll:
    Introduce additional filter for election status

--- client/src/component/ElectionList.js
+++ client/src/component/ElectionList.js
@@ -3,16 +3,20 @@ import {StateContext} from "../context/context";
 import {Container, Col, Row} from "react-bootstrap";
 import ElectionCard from "./ElectionCard";
 import ElectionCategoryDD from "./dropdown/ElectionCategoryDD";
+import ElectionStatusDD from "./dropdown/ElectionStatusDD";
 
 export default function ElectionList() {
 
     const {state} = useContext(StateContext)
     const {elections} = state;
     const [category, setCategory] = useState("All")
+    const [electionStatus, setElectionStatus] = useState("OPEN");
 
-    const handleOnSelect = (eventKey) => setCategory(eventKey);
+    const handleOnSelectCategory = (eventKey) => setCategory(eventKey);
+    const handleOnSelectStatus = (eventKey) => setElectionStatus(eventKey);
 
-    const filterSwitch = c => {
+
+    const filterCategorySwitch = c => {
         switch (category) {
             case 'All':
                 return true;
@@ -21,18 +25,28 @@ export default function ElectionList() {
         }
     }
 
+    const filterStatusSwitch = election => {
+        switch (electionStatus) {
+            case "OPEN":
+                return election.status === "OPEN";
+            default:
+                return election.status === "CLOSED";
+        }
+    }
+
     return (
         <>
             <Container>
                 <Row className="mt-1 mb-1">
                     <Col>
-                        <ElectionCategoryDD category={category} onSelect={handleOnSelect}/>
+                        <ElectionCategoryDD category={category} onSelect={handleOnSelectCategory}/>
+                        <ElectionStatusDD status={electionStatus} onSelect={handleOnSelectStatus}/>
                     </Col>
                 </Row>
                 <div className="d-flex flex-wrap justify-content-start">
                     {
                         elections &&
-                        elections.filter(filterSwitch).map((election) => (
+                        elections.filter(filterCategorySwitch).filter(filterStatusSwitch).map((election) => (
                             <div key={election.id}>
                                 <ElectionCard election={election}/>
                             </div>
