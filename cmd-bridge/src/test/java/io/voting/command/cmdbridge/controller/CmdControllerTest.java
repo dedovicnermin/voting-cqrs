8b670fa672d67b57cee10a33f71b21e9c594cc63
Nermin Dedovic <dedovicnermin@outlook.com>
2024-05-08 15:23:37 -0500

    feature/remove-restful-produce:
    Include simple integration test for cmd-bridge component

--- cmd-bridge/src/test/java/io/voting/command/cmdbridge/controller/CmdControllerTest.java
+++ cmd-bridge/src/test/java/io/voting/command/cmdbridge/controller/CmdControllerTest.java
@@ -1,8 +1,16 @@
 package io.voting.command.cmdbridge.controller;
 
+import io.cloudevents.CloudEvent;
 import io.voting.command.cmdbridge.config.AppConfig;
+import io.voting.command.cmdbridge.controller.framework.TestConsumerHelper;
+import io.voting.command.cmdbridge.controller.framework.TestKafkaContext;
+import io.voting.common.library.kafka.models.ReceiveEvent;
+import io.voting.common.library.kafka.utils.CloudEventTypes;
 import io.voting.common.library.models.ElectionCreate;
 import io.voting.common.library.models.ElectionVote;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +18,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.rsocket.server.LocalRSocketServerPort;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.net.URI;
 import java.util.Arrays;
-
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
-class CmdControllerTest {
+class CmdControllerTest extends TestKafkaContext {
 
+  private static TestConsumerHelper consumerHelper;
   private static RSocketRequester requester;
+
+
+  @DynamicPropertySource
+  static void registerKafkaProperties(final DynamicPropertyRegistry dynamicPropertyRegistry) {
+    dynamicPropertyRegistry.add("kafka.properties.bootstrap.servers", kafkaContainer::getBootstrapServers);
+  }
 
   @BeforeAll
   public static void setupOnce(
@@ -31,8 +48,15 @@ class CmdControllerTest {
             .rsocketStrategies(strategies)
             .connectWebSocket(URI.create("ws://localhost:" + port + "/cmd"))
             .block();
+    consumerHelper = new TestConsumerHelper(kafkaContainer);
   }
 
+  @AfterEach
+  void cleanup() {
+    consumerHelper.clearQueues();
+  }
+
+  @SneakyThrows
   @Test
   void testVoteCmd() {
     final Mono<Void> result = requester
@@ -44,8 +68,12 @@ class CmdControllerTest {
     StepVerifier
             .create(result)
             .verifyComplete();
+    final ReceiveEvent<String, CloudEvent> element = consumerHelper.getEvents().poll(3, TimeUnit.SECONDS);
+    Assertions.assertThat(element.getKey()).isEqualTo("777:888");
+    Assertions.assertThat(element.getPOrE().getPayload().getType()).isEqualTo(CloudEventTypes.ELECTION_VOTE_CMD);
   }
 
+  @SneakyThrows
   @Test
   void testElectionCmd() {
     final Mono<Void> result = requester
@@ -57,7 +85,9 @@ class CmdControllerTest {
     StepVerifier
             .create(result)
             .verifyComplete();
+
+    final ReceiveEvent<String, CloudEvent> element = consumerHelper.getEvents().poll(3, TimeUnit.SECONDS);
+    Assertions.assertThat(element.getKey()).isEqualTo("777");
+    Assertions.assertThat(element.getPOrE().getPayload().getType()).isEqualTo(CloudEventTypes.ELECTION_CREATE_CMD);
   }
-
-
 }
